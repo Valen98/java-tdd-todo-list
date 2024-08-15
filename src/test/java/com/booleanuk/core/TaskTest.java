67dbe15fe7f856007b13560bac0324dc7d1cf49e
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-15 10:35:25 +0200

    Testing to get task name, red test

---
+++ src/test/java/com/booleanuk/core/TaskTest.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    @Test
+    public void createTaskTest() {
+        Task task = new Task();
+
+        Assertions.assertEquals("First Task", task.getName(task));
+    }
+}
