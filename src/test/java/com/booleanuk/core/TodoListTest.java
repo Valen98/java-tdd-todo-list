9d478eed5778d72d9f437e1f482b5ef1c6f9db7f
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-15 11:21:17 +0200

    Added a viewTodoList to see if I can view the list

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -3,6 +3,9 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class TodoListTest {
 
     @Test
@@ -12,5 +15,21 @@ class TodoListTest {
 
         Assertions.assertTrue(todoList.add(t1));
         Assertions.assertFalse(todoList.add(t1));
+    }
+
+    @Test
+    public void viewTodoList() {
+        TodoList todoList = new TodoList();
+        ArrayList<Task> testList = new ArrayList<>();
+        Task t1 = new Task("First Task");
+        Task t2 = new Task("Second Task");
+
+
+        todoList.add(t1);
+        todoList.add(t2);
+        testList.add(t1);
+        testList.add(t2);
+
+        Assertions.assertEquals(testList, todoList.viewList());
     }
 }
