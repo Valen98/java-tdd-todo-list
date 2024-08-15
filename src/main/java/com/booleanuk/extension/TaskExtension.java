18ab39460631b2b2c22cf87a5e5d22d823470716
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-15 16:16:32 +0200

    Added domain model and added tests for tasks

---
+++ src/main/java/com/booleanuk/extension/TaskExtension.java
@@ -0,0 +1,35 @@
+package com.booleanuk.extension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class TaskExtension {
+    int id;
+    String name;
+    boolean isComplete;
+    LocalDateTime date;
+
+    public TaskExtension(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.isComplete = false;
+        this.date = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDateString() {
+        return date.toString();
+    }
+
+    public boolean getTaskStatus() {
+        return this.isComplete;
+    }
+
+    public void changeStatus() {
+        this.isComplete = !this.isComplete;
+    }
+}
---
+++ src/main/java/com/booleanuk/extension/TodoListExtension.java
@@ -0,0 +1,15 @@
+package com.booleanuk.extension;
+
+import com.booleanuk.core.Task;
+
+import java.util.ArrayList;
+
+public class TodoListExtension {
+    ArrayList<Task> todoList;
+
+    public TodoListExtension() {
+        this.todoList = new ArrayList<>();
+    }
+
+    
+}
