b3bbb2204c8591e4e15c2c6ebd18a9f34a05cb1f
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-15 11:06:08 +0200

    Added a green test to add a task

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,6 +1,21 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class TodoList {
 
+    ArrayList<Task> todoList;
 
+    public TodoList() {
+        this.todoList = new ArrayList<>();
+    }
+
+    public boolean add(Task task) {
+        if(this.todoList.contains(task)) {
+            return false;
+        }else {
+            this.todoList.add(task);
+            return true;
+        }
+    }
 }
